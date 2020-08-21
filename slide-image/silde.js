0808db936d1fa1434d35425286ebd40b4763621c
minhoe <minhoe@netman.co.kr>
2020-08-21 18:01:40 +0900

    Add slide img example

---
+++ slide-image/silde.js
@@ -0,0 +1,13 @@
+const iconList = [
+  '<i class="fa fa-windows" aria-hidden="true"></i>',
+  '<i class="fa fa-apple" aria-hidden="true"></i>',
+  '<i class="fa fa-linux" aria-hidden="true"></i>',
+  '<i class="fa fa-amazon" aria-hidden="true"></i>',
+  '<i class="fa fa-bitbucket" aria-hidden="true"></i>',
+  '<i class="fa fa-firefox" aria-hidden="true"></i>',
+  '<i class="fa fa-opera" aria-hidden="true"></i>',
+  '<i class="fa fa-git" aria-hidden="true"></i>',
+];
+const slide = ((icons) => {
+  return '';
+})(iconList);
