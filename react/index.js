4426819b4415bec10c99da4b041b131852bed121
minemanemo <minhoe1122@gmail.com>
2020-09-08 23:51:27 +0900

    Add render by by vanilla js

--- react/index.js
+++ react/index.js
@@ -1,6 +1,29 @@
 /** @jsx createElement */
 // 위 부분은 React.createElement를 template 대체 할 수 있다.
 
+function renderElement(node) {
+  // type에 해당하는 element를 생성.
+  const el = document.createElement(node.type);
+
+  // 하위 element를 render한 후 하위에 추가해 준다.
+  // 재귀 함수를 호출하도록 하여 마지막 노드까지 랜더링
+  node.children.map(renderElement).forEach((element) => {
+    el.appendChild(element);
+  });
+
+  // 생성된 Element를 반환.
+  return el;
+}
+
+function render(newVdom, container) {
+  // vdom vs newVdom : diff algorithm
+  // 실제로 React는 해당 포인트에서
+  // 기존의 old Virtual DOM과 new Virtual DOM의 차이점을
+  // 찾아서 변경된 부분만 실제 Real DOM에서 새롭게 rendering 한다.
+
+  container.appendChild(renderElement(newVdom));
+}
+
 function createElement(type, props = {}, ...children) {
   // type이 funtion인 경우 예외 처리
   if (typeof type === "function") {
